04fb8826a30d8dda1c5830cfcd13c8f345c06dda
student <student@iMac-68.local>
2018-12-12 17:46:35 +0800

    kk

---
+++ h5/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  h5
+//
+//  Created by student on 2018/12/12.
+//  Copyright © 2018年 student. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Third"
+        self.view.backgroundColor = UIColor.purple
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
